5a538e1bfd09a71c06d83070d75efe87bd4951a9
Eskil Berg Ould-Saada <eskilbo@stud.ntnu.no>
2020-02-27 13:13:28 +0100

    added sigint_handler

--- skeleton_project/source/main.c
+++ skeleton_project/source/main.c
@@ -1,8 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "elevator.h"
+#include "signal.h"
+
+static void sigint_handler(int sig){
+    (void)(sig);
+    printf("Terminating elevator\n");
+    hardware_command_movement(HARDWARE_MOVEMENT_STOP);
+    exit(0);
+}
 
 int main(){
+    signal(SIGINT, sigint_handler);
     run_elevator();
     return 0;
 }
